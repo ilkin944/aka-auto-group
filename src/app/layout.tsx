8eef9df7930465b3f0a5e4e2383683bcadf8e97a
Ilkin <ilkin944@gmail.com>
2025-08-20 17:19:28 +0400

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Roboto({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+});
+
+export const metadata: Metadata = {
+  title: "Locarise | Your Trusted Partner",
+  description: "Locarise - Your Trusted Partner for Fast and Reliable Services",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
